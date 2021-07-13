c61abfc95f100a30013337dff04f506f412aeca4
Rishi-Dave <62260675+Rishi-Dave@users.noreply.github.com>
2021-07-13 09:28:50 -0700

    Update views.py

--- ewaste/ewasteapp/views.py
+++ ewaste/ewasteapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
 # Create your views here.
 def home_page(request):
     return HttpResponse('polls/templates/index.html')
+class HomeView(TemplateView):
+
+    template_name = 'index.html'
