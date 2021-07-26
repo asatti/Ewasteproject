9a49b654dbeed631b08c450d1cc3de3c9db464d2
Rishi-Dave <62260675+Rishi-Dave@users.noreply.github.com>
2021-07-26 09:48:02 -0700

    Update views.py

--- ewaste/ewasteapp/views.py
+++ ewaste/ewasteapp/views.py
@@ -4,11 +4,6 @@ from django.views.generic.base import TemplateView
 from django.views import generic
 
 # Create your views here.
-#def home_page(request):
-    #return HttpResponse(index.html)
-from django.views.generic.base import TemplateView
-
-
-class HomeView(TemplateView):
-
-    template_name = 'index.html'
+def home_page(request):
+    return render(request, "index.html")
+ 
