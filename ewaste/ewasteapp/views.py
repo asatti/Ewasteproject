0eca163edfb9f68f83abee91d329c940ae4eb7bd
Rishi-Dave <62260675+Rishi-Dave@users.noreply.github.com>
2021-08-08 11:09:48 -0700

    Update views.py

--- ewaste/ewasteapp/views.py
+++ ewaste/ewasteapp/views.py
@@ -3,7 +3,19 @@ from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views import generic
 
+from ewasteapp.forms import MyModelForm
+from .models import Item
 # Create your views here.
 def home_page(request):
     return render(request, "index.html")
- 
+class ItemPickupView(generic.ListView):
+    model = Item
+    form_class = MyModelForm
+    template_name = 'pickup.html'
+    success_url = 'success.html'
+def pickup(request):
+    return render(request, 'pickup.html',)
+def load_items(request):
+    
+    branches = Item.objects.filter().order_by('name')
+    return render(request, 'user/branch_dropdown_list_options.html', {'branches': branches})
