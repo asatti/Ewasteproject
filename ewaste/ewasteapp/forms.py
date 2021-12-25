8ed4a8e44c08aadd8a2a291ab3eb78436b293359
Rishi-Dave <rishipdave@gmail.com>
2021-12-25 10:59:14 -0800

    form for signup

--- ewaste/ewasteapp/forms.py
+++ ewaste/ewasteapp/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
 from .models import Item
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 class objectTypeForm(ModelForm):
     class Meta:
         model = Item
-        #fields = ['object_type']
+        fields = ['object_type']
+
+class SignUpForm(ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'first_name', 'last_name', 'address')
+
 
 
--- ewaste/ewasteapp/views.py
+++ ewaste/ewasteapp/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
-
-from ewasteapp.forms import  objectTypeForm
+from ewasteapp.forms import  objectTypeForm, SignUpForm
 from .models import Item
 # Create your views here.
 def home_page(request):
@@ -13,6 +12,10 @@ class ItemPickupView(CreateView):
     form_class = objectTypeForm
     template_name = 'pickup.html'
     success_url = 'pickup.html'
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = 'login.html'
+    template_name = 'signup.html'
 def pickup(request):
     return render(request, 'pickup.html',)
 
